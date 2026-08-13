cb4c5e117eaacff0994efb99f85975a2e7b5df39
Itamar Turner-Trauring <itamar@itamarst.org>
2026-08-13 13:08:01 -0400

    Revert "Expose API to allow other thread pools to limit cores."
    This reverts commit 429f11a27ebde957c0fa885a76d5189abd9d5089.

--- joblib/parallel.py
+++ joblib/parallel.py
@@ -35,7 +35,6 @@ from ._parallel_backends import (
     ParallelBackendBase,  # noqa
     SequentialBackend,
     ThreadingBackend,
-    set_thread_cores_limit,  # noqa
 )
 from ._utils import _Sentinel, eval_expr
 from .disk import memstr_to_bytes
